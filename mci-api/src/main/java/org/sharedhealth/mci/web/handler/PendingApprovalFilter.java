0288317510f6f61355a5f3fad84168638a5a4bfd
Utsab Banerjee <utsabban@thoughtworks.com>
2015-05-05 12:53:32 +0530

    Utsab | BDSHR - 721 | removing approvals for active and merged_with

--- mci-api/src/main/java/org/sharedhealth/mci/web/handler/PendingApprovalFilter.java
+++ mci-api/src/main/java/org/sharedhealth/mci/web/handler/PendingApprovalFilter.java
@@ -29,7 +29,7 @@ public class PendingApprovalFilter {
 
     private static final String NEEDS_APPROVAL = "NA";
     private static final String NON_UPDATABLE = "NU";
-//    public static final String EMPTY_VALUE = "";
+
 
     private PatientData newPatient;
     private Requester requestedBy;
@@ -100,20 +100,6 @@ public class PendingApprovalFilter {
     }
 
     private PatientActivationInfo processPatientActivationInfo(String key, PatientActivationInfo oldValue, PatientActivationInfo newValue) {
-//        if (null == newValue) {
-//            return oldValue;
-//        }
-//        Object active = process(ACTIVE, oldValue.getActive(), newValue.getActive());
-//        Object mergedWith = null;
-//        if (null != newValue.getActive() && newValue.getActive()) {
-//            mergedWith = process(MERGED_WITH, oldValue.getMergedWith(), EMPTY_VALUE);
-//        }
-//        if (null != newValue.getActive() && !newValue.getActive()) {
-//            mergedWith = process(MERGED_WITH, oldValue.getMergedWith(), newValue.getMergedWith());
-//        }
-//        newValue.setActive(active == null ? null : (Boolean) active);
-//        newValue.setMergedWith(mergedWith == null ? null : (String) mergedWith);
-//        return newValue;
         Object patientActivationInfo = process(key, oldValue, newValue);
         return patientActivationInfo == null ? null : (PatientActivationInfo) patientActivationInfo;
     }
